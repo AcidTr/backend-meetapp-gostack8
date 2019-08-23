bf3192a3a5343c56f27f3ec63f4242477c640774
Victor Mourão <victor_murati@hotmail.com>
2019-08-23 00:12:57 -0300

    first commit with initial configuration

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '<password>',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
