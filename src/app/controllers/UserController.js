f2576a71543e24e2f5e04bc4a171453f5598c5a5
Victor Mourão <victor_murati@hotmail.com>
2019-08-24 15:25:10 -0300

    Update user data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,34 @@ class UserController {
       email,
     });
   }
+
+  //Function to update user data
+  async update(req, res) {
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userEmailExits = await User.findOne({ where: { email: req.body.email } });
+
+      if (userEmailExits) {
+        return res.status(400).json({ error: "User email already exists" });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      email,
+      name
+    });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,7 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+
+routes.put('/users', UserController.update);
+
 export default routes;
